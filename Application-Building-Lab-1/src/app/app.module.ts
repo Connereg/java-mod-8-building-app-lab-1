f238ed192b863c567b35f7e376893b39fb924641
Conner Gleason <connereg@optonline.net>
2022-08-18 11:43:37 -0400

    Completed Lab

--- Application-Building-Lab-1/src/app/app.module.ts
+++ Application-Building-Lab-1/src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { ConversationHistoryComponentComponent } from './conversation-history-component/conversation-history-component.component';
 import { ConversationThreadComponentComponent } from './conversation-thread-component/conversation-thread-component.component';
 import { ContactListComponentComponent } from './contact-list-component/contact-list-component.component';
+import { ConversationControlComponentComponent } from './conversation-control-component/conversation-control-component.component';
+import { ContactComponentComponent } from './contact-component/contact-component.component';
+import { SendMessageComponentComponent } from './send-message-component/send-message-component.component';
+import { UserMessageComponentComponent } from './user-message-component/user-message-component.component';
+import { SenderMessageComponentComponent } from './sender-message-component/sender-message-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConversationHistoryComponentComponent,
     ConversationThreadComponentComponent,
-    ContactListComponentComponent
+    ContactListComponentComponent,
+    ConversationControlComponentComponent,
+    ContactComponentComponent,
+    SendMessageComponentComponent,
+    UserMessageComponentComponent,
+    SenderMessageComponentComponent
   ],
   imports: [
     BrowserModule,
